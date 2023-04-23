ec11fa0216275e5f1408767dfdc72c310ce2fda8
Davit Dzneladze <davitdzeladze95@gmail.com>
2023-04-23 23:37:42 +0400

    Fix naming

---
+++ src/users/users.module.ts
@@ -0,0 +1,15 @@
+import { Module, forwardRef } from "@nestjs/common";
+import { SequelizeModule } from "@nestjs/sequelize";
+
+import { UsersController } from "./users.controller";
+import { UsersService } from "./users.service";
+import { User } from "./users.model";
+import { CreateUserDto } from "./dto/create-user.dto";
+
+@Module({
+  controllers: [UsersController],
+  providers: [UsersService],
+  imports: [SequelizeModule.forFeature([User])],
+  exports: [UsersService],
+})
+export class UsersModule {}
