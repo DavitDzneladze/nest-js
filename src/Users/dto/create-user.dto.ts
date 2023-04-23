9aa47a76ee0442d9a9b1f69d9098815eeb500ef0
Davit Dzneladze <davitdzeladze95@gmail.com>
2023-04-23 23:30:52 +0400

    fix

--- src/Users/dto/create-user.dto.ts
+++
@@ -1,9 +0,0 @@
-import { ApiProperty } from "@nestjs/swagger";
-
-export class CreateUserDto {
-  @ApiProperty({ example: "email@gmail.com", description: "user email" })
-  readonly email: string;
-
-  @ApiProperty({ example: "12345", description: "password" })
-  readonly password: string;
-}
